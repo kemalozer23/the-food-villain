82e7f11ae6cb41547e7f90fb0dc87e3dd94f5337
kemalozer23 <89297908+kemalozer23@users.noreply.github.com>
2022-11-14 21:35:18 +0300

    data

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 8888;
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send({
-        ALC: 'The Food Villain'
-    })
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
 
-app.listen(5000)
+const AuthRoutes = require('./routes/authRoutes.js');
+app.use('/api', cors(), AuthRoutes);
+
+const getHashParams = () => {
+  const hashParams = {};
+  let e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.search.substring(1);
+
+  while (e = r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+
+  return hashParams;
+}
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
